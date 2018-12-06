2123ec474fae598f0bd62f2e0af0583f3840ea3c
wmay <williamcmay@live.com>
2018-12-06 13:31:09 -0500

    Add autocals table and loading function

--- postgres/setup_general.sql
+++ postgres/setup_general.sql
@@ -1,5 +1,14 @@
 /* Creating tables used by multiple instruments */
 
+create table autocals (
+  station_id int references stations,
+  instrument text,
+  dates daterange,
+  start_time time,
+  end_time time,
+  primary key(station_id, instrument, dates, start_time, end_time)
+);
+
 create table clock_audits (
   instrument text,
   instrument_time timestamp,
