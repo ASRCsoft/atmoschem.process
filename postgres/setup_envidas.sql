dc4d89ca2443a6dd78f4c8a3f99c482cdfca67bb
wmay <williamcmay@live.com>
2018-12-14 17:46:55 -0500

    Convert envidas table to timescaledb hypertable
    This vastly speeds up queries that search by the instrument time. The
    tradeoff is that I can no longer keep station_id/sourcerow as the
    official primary key. But I can adequately enforce unique constraints
    on those columns when loading data, so it's a small loss.

--- postgres/setup_envidas.sql
+++ postgres/setup_envidas.sql
@@ -1,20 +1,22 @@
 /* Organizing various data contained in envidas files */
 
 create extension hstore;
+CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
 create table envidas (
-  station_id int references stations,
-  source sourcerow,
-  instrument_time timestamp,
+  station_id int references stations not null,
+  source sourcerow not null,
+  instrument_time timestamp not null,
   temperature numeric,
   rh numeric,
   bp numeric,
   no numeric,
   co numeric,
   so2 numeric,
-  data_dict hstore,
-  primary key(station_id, source)
+  data_dict hstore
 );
-/* create index useful for subsetting based on recorded instrument
-   time (often used for displaying the raw data) */
-CREATE INDEX raw_envidas_time ON envidas(instrument_time);
+SELECT create_hypertable('envidas', 'instrument_time');
+/* create index useful for subsetting based on station and recorded
+   instrument time (used when plotting the raw data and when running
+   processing queries) */
+CREATE INDEX envidas_station_raw_time ON envidas(station_id, instrument_time);
