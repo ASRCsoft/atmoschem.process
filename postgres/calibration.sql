97347f50db5fc9d5d4a420b769e14a528c8848a8
wmay <williamcmay@live.com>
2018-12-14 18:04:06 -0500

    Clean up estimate_zero_val and have it use timescaledb index

--- postgres/calibration.sql
+++ postgres/calibration.sql
@@ -4,22 +4,21 @@ create or replace function estimate_zero_val(val text, times timerange, cal_date
   declare
   exec_str text = $exec$
     with moving_averages as (
-      select instrument_time,
-	     AVG(%I) OVER(ORDER BY instrument_time
+      select AVG(%I) OVER(ORDER BY instrument_time
 			  ROWS BETWEEN 2 PRECEDING AND 2 following) as moving_average
 	from envidas
-	       join autocals
-		   on instrument_time::date=$2
-	       and instrument_time::time <@ $1
-	       and envidas.station_id=$3
+       where instrument_time between $1 and $2
+	 and envidas.station_id=$3
     )
     select min(moving_average) from moving_averages;
   $exec$;
+  start_time timestamp = cal_date + lower(times);
+  end_time timestamp = cal_date + upper(times);
   zero_estimate numeric;
 begin
   execute format(exec_str, val)
     into zero_estimate
-    using times, cal_date, site;
+    using start_time, end_time, site;
   RETURN zero_estimate;
 end;
 $$ language plpgsql;
