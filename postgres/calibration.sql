b05364c2a8467826c7af31e0b8031c95da1e1a24
wmay <williamcmay@live.com>
2018-12-17 17:15:02 -0500

    Add estimate_max_val for calibration periods, rename zero to min

--- postgres/calibration.sql
+++ postgres/calibration.sql
@@ -1,6 +1,6 @@
 /* Instrument calibration */
 
-create or replace function estimate_zero_val(val text, times timerange, cal_date date, site int) returns numeric as $$
+create or replace function estimate_min_val(val text, times timerange, cal_date date, site int) returns numeric as $$
   declare
   exec_str text = $exec$
     with moving_averages as (
@@ -22,3 +22,28 @@ begin
   RETURN zero_estimate;
 end;
 $$ language plpgsql;
+
+create or replace function estimate_max_val(val text, times timerange, cal_date date, site int) returns numeric as $$
+  declare
+  exec_str text = $exec$
+    with moving_averages as (
+      select AVG(%I) OVER(ORDER BY instrument_time
+			  ROWS BETWEEN 2 PRECEDING AND 2 following) as moving_average
+	from envidas
+       where instrument_time between $1 and $2
+	 and envidas.station_id=$3
+    )
+    select max(moving_average) from moving_averages;
+  $exec$;
+  start_time timestamp = cal_date + lower(times);
+  end_time timestamp = cal_date + upper(times);
+  zero_estimate numeric;
+begin
+  execute format(exec_str, val)
+    into zero_estimate
+    using start_time, end_time, site;
+  RETURN zero_estimate;
+end;
+$$ language plpgsql;
+
+/* After getting these calibration values, need to use linear interpolation to get zero/top for every measurement time, then correct each measurement */
