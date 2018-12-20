b4993b119ac5d060277eae502b22db3794d6630a
wmay <williamcmay@live.com>
2018-12-20 11:55:40 -0500

    Accept timestamp x values in interpolate function

--- postgres/utilities.sql
+++ postgres/utilities.sql
@@ -17,33 +17,33 @@ CREATE TYPE sourcerange AS RANGE (
 
 /* estimate the y-value between x0 and x1 using linear interpolation
 */
-CREATE OR REPLACE FUNCTION interpolate(x0 numeric, x1 numeric, y0 numeric, y1 numeric, x numeric)
+CREATE OR REPLACE FUNCTION interpolate(t0 timestamp, t1 timestamp, y0 numeric, y1 numeric, t timestamp)
   RETURNS numeric AS $$
   DECLARE
   has_lower_bound boolean;
   has_upper_bound boolean;
-BEGIN
-  if x0 is not null and y0 is not null then
-    select true
+  BEGIN
+    if t0 is not null and y0 is not null then
+      select true
       into has_lower_bound;
-  end if;
-  if x1 is not null and y1 is not null then
-    select true
-    into has_upper_bound;
-  end if;
-  -- Return the appropriate estimate
-  if has_lower_bound and has_upper_bound then
-    -- interpolate
-    return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
-  elsif has_lower_bound then
-    -- go with the lowest value
-    return y0;
-  elsif has_upper_bound then
-    -- go with the highest value
-    return y1;
-  else
-    -- return the original time
-    return t;
-  end if;
-END;
+    end if;
+    if t1 is not null and y1 is not null then
+      select true
+      into has_upper_bound;
+    end if;
+    -- Return the appropriate estimate
+    if has_lower_bound and has_upper_bound then
+      -- interpolate
+      return y0 + extract(epoch from (t - t0)) * (y1 - y0) / extract(epoch from (t1 - t0));
+    elsif has_lower_bound then
+      -- go with the lowest value
+      return y0;
+    elsif has_upper_bound then
+      -- go with the highest value
+      return y1;
+    else
+      -- no interpolation to return
+      return null;
+    end if;
+  END;
 $$ LANGUAGE plpgsql;
