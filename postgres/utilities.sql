00e3ffee59af0316f6f99908d5cd77b5b3addcf1
wmay <williamcmay@live.com>
2018-12-31 15:43:26 -0500

    Overload interpolate function to allow timestamp x values

--- postgres/utilities.sql
+++ postgres/utilities.sql
@@ -16,25 +16,25 @@ CREATE TYPE sourcerange AS RANGE (
 );
 
 /* estimate the y-value between x0 and x1 using linear interpolation
-*/
-CREATE OR REPLACE FUNCTION interpolate(t0 timestamp, t1 timestamp, y0 numeric, y1 numeric, t timestamp)
+ */
+CREATE OR REPLACE FUNCTION interpolate(x0 numeric, x1 numeric, y0 numeric, y1 numeric, x numeric)
   RETURNS numeric AS $$
   DECLARE
   has_lower_bound boolean;
   has_upper_bound boolean;
   BEGIN
-    if t0 is not null and y0 is not null then
+    if x0 is not null and y0 is not null then
       select true
       into has_lower_bound;
     end if;
-    if t1 is not null and y1 is not null then
+    if x1 is not null and y1 is not null then
       select true
       into has_upper_bound;
     end if;
     -- Return the appropriate estimate
     if has_lower_bound and has_upper_bound then
       -- interpolate
-      return y0 + extract(epoch from (t - t0)) * (y1 - y0) / extract(epoch from (t1 - t0));
+      return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
     elsif has_lower_bound then
       -- go with the lowest value
       return y0;
@@ -45,5 +45,16 @@ CREATE OR REPLACE FUNCTION interpolate(t0 timestamp, t1 timestamp, y0 numeric, y
       -- no interpolation to return
       return null;
     end if;
+  END;
+$$ LANGUAGE plpgsql;
+
+/* overload to allow timestamps as x values */
+CREATE OR REPLACE FUNCTION interpolate(t0 timestamp, t1 timestamp, y0 numeric, y1 numeric, t timestamp)
+  RETURNS numeric AS $$
+  BEGIN
+    return interpolate(extract(epoch from t0)::numeric,
+		       extract(epoch from t1)::numeric,
+		       y0, y1,
+		       extract(epoch from t)::numeric);
   END;
 $$ LANGUAGE plpgsql;
