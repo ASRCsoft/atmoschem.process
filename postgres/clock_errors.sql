b1631298f2612696e27b7135493147cb3a8e02a9
wmay <williamcmay@live.com>
2019-01-10 15:09:15 -0500

    Include station_id in correct_instrument_time function

--- postgres/clock_errors.sql
+++ postgres/clock_errors.sql
@@ -38,7 +38,7 @@ CREATE MATERIALIZED VIEW matched_clock_audits as
 
 
 /* Correct ultrafine instrument time using linear interpolation */
-CREATE OR REPLACE FUNCTION correct_instrument_time(instrument text, sr sourcerow, t timestamp)
+CREATE OR REPLACE FUNCTION correct_instrument_time(station_id int, instrument text, sr sourcerow, t timestamp)
   RETURNS timestamp AS $$
   #variable_conflict use_variable
   DECLARE
@@ -47,38 +47,42 @@ CREATE OR REPLACE FUNCTION correct_instrument_time(instrument text, sr sourcerow
   x1 timestamp;
   y0 interval;
   y1 interval;
-BEGIN
-  -- 1) Find the closest clock audits.
-  select audit_time
-    from matched_clock_audits
-   where matched_clock_audits.instrument=instrument
-     and data_source=sr
-    into matching_audit_time;
-  if found then
-    return matching_audit_time;
-  end if;
-  -- if the clock error was corrected, then the new instrument_time is
-  -- the same as the audit_time and the new clock error is zero. If it
-  -- wasn't corrected then the instrument_time and clock errors don't
-  -- change.
-  select case when corrected then audit_time
-         else instrument_time end,
-         case when corrected then '0'
-         else audit_time - instrument_time end
-    from matched_clock_audits
-   where matched_clock_audits.instrument=instrument
-     and data_source<sr
-   order by data_source desc
-   limit 1
-    into x0, y0;
-  select instrument_time,
-    	 audit_time - instrument_time
-    from matched_clock_audits
-   where matched_clock_audits.instrument=instrument
-     and data_source>sr
-   order by data_source asc
-   limit 1
-    into x1, y1;
-  return t + interpolate(x0, x1, y0, y1, t);
-END;
+  BEGIN
+    -- only applies to PSP for now
+    if station_id!=3 then
+      return t;
+    end if;
+    -- 1) Find the closest clock audits.
+    select audit_time
+      from matched_clock_audits
+     where matched_clock_audits.instrument=instrument
+       and data_source=sr
+      into matching_audit_time;
+    if found then
+      return matching_audit_time;
+    end if;
+    -- if the clock error was corrected, then the new instrument_time is
+    -- the same as the audit_time and the new clock error is zero. If it
+    -- wasn't corrected then the instrument_time and clock errors don't
+    -- change.
+    select case when corrected then audit_time
+           else instrument_time end,
+           case when corrected then '0'
+           else audit_time - instrument_time end
+      from matched_clock_audits
+     where matched_clock_audits.instrument=instrument
+       and data_source<sr
+     order by data_source desc
+     limit 1
+      into x0, y0;
+    select instrument_time,
+	   audit_time - instrument_time
+      from matched_clock_audits
+     where matched_clock_audits.instrument=instrument
+       and data_source>sr
+     order by data_source asc
+     limit 1
+      into x1, y1;
+    return t + interpolate(x0, x1, y0, y1, t);
+  END;
 $$ LANGUAGE plpgsql;
