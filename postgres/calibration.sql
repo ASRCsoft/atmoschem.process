af7b9658ea1ecc18b0eb583a6df1cac72dc756a3
wmay <williamcmay@live.com>
2019-01-15 14:22:33 -0500

    Move autocal code to calibration.sql

--- postgres/calibration.sql
+++ postgres/calibration.sql
@@ -1,5 +1,46 @@
 /* Instrument calibration */
 
+create table autocals (
+  station_id int references stations,
+  instrument text,
+  type text,
+  dates daterange,
+  times timerange,
+  primary key(station_id, instrument, dates, times)
+);
+
+/* Load autocal schedules into the autocals table */
+CREATE OR REPLACE FUNCTION load_autocals(file text)
+  RETURNS void AS $$
+  declare
+  copy_str text;
+  BEGIN
+    /* this temporary table will hold a copy of the data file */
+    create temporary table autocals_file (
+      station text,
+      instrument text,
+      type text,
+      start_date date,
+      end_date date,
+      start_time time,
+      end_time time
+    ) on commit drop;
+select format('COPY autocals_file FROM ''%s'' delimiter '','' csv header',
+	      file)
+  into copy_str;
+EXECUTE copy_str;
+INSERT INTO autocals
+SELECT (select id
+	  from stations
+	 where short_name=station),
+       instrument,
+       type,
+       daterange(start_date, end_date, '[]'),
+       timerange(start_time, end_time, '[]')
+  FROM autocals_file;
+END;
+$$ LANGUAGE plpgsql;
+
 create or replace function estimate_min_val(val text, times timerange, cal_date date, site int) returns numeric as $$
   declare
   exec_str text = $exec$
