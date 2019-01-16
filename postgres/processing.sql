8da1a99cc99c6d49e3e358fdfa2ba50bedd5dff6
wmay <williamcmay@live.com>
2019-01-15 19:20:06 -0500

    Add relevant columns to calibrated_envidas

--- postgres/processing.sql
+++ postgres/processing.sql
@@ -2,8 +2,10 @@
 /* Get calibrated envidas data */
 CREATE materialized VIEW calibrated_envidas AS
   select station_id,
+	 source,
 	 corrected_time as time,
-	 apply_calib(station_id, 'NO', no, corrected_time) as no
+	 apply_calib(station_id, 'NO', no, corrected_time) as no,
+	 data_dict->'no_flag' as no_flag
     from (select *,
 		 correct_instrument_time(station_id, 'DRDAS PC', source, instrument_time) as corrected_time
 	    from envidas) e1;
@@ -12,9 +14,11 @@ CREATE materialized VIEW calibrated_envidas AS
 CREATE materialized VIEW filtered_envidas AS
   select station_id,
 	 time,
-	 case when (no - median_no) / mad_no > 3 then null
-	 else no end as no
+	 no,
+	 is_flagged('NO', station_id, source, time,
+		    no, no_flag, median_no, mad_no) as no_flagged
     from (select station_id,
+		 source,
 		 time,
 		 no,
 		 median(no) over w as median_no,
