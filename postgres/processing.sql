ecf0dd21150fde018b976f573ad736c7e370011b
wmay <williamcmay@live.com>
2019-02-22 17:21:59 -0500

    Update processing code to use measurement type IDs

--- postgres/processing.sql
+++ postgres/processing.sql
@@ -16,12 +16,11 @@ constraints. */
 These views apply calibration adjustments if needed. They rely on
 functions from calibration.sql. */
 CREATE or replace VIEW calibrated_measurements as
-  select c.site_id,
+  select c.measurement_type_id,
 	 instrument_time,
 	 record,
-	 c.measurement,
 	 value,
-	 case when has_calibration then apply_calib(c.site_id, c.measurement, value, instrument_time)
+	 case when has_calibration then apply_calib(c.measurement_type_id, value, instrument_time)
 	 else value end as calibrated_value,
 	 flagged,
 	 valid_range,
@@ -29,20 +28,23 @@ CREATE or replace VIEW calibrated_measurements as
 	 remove_outliers
     from measurements c
 	   left join measurement_types m
-	       on c.site_id=m.site_id
-	       and c.measurement=m.measurement;
+	       on c.measurement_type_id=m.id;
 
 CREATE materialized VIEW calibrated_campbell_wfms as
   select *
     from calibrated_measurements
-   where site_id=1;
-create index calibrated_campbell_wfms_idx on calibrated_campbell_wfms(site_id, measurement, instrument_time);
+   where measurement_type_id in (select id
+				   from measurement_types
+				  where site_id=1);
+create index calibrated_campbell_wfms_idx on calibrated_campbell_wfms(measurement_type_id, instrument_time);
 
 CREATE materialized VIEW calibrated_campbell_wfml as
   select *
     from calibrated_measurements
-   where site_id=2;
-create index calibrated_campbell_wfml_idx on calibrated_campbell_wfml(site_id, measurement, instrument_time);
+   where measurement_type_id in (select id
+				   from measurement_types
+				  where site_id=2);
+create index calibrated_campbell_wfml_idx on calibrated_campbell_wfml(measurement_type_id, instrument_time);
 
 /* 2) Running medians.
 
@@ -56,10 +58,10 @@ CREATE materialized VIEW campbell_medians_wfms AS
 	 case when remove_outliers then runmad(calibrated_value) over w
 	 else null end as running_mad
     from calibrated_campbell_wfms
-  WINDOW w AS (partition by site_id, measurement
+  WINDOW w AS (partition by measurement_type_id
 	       ORDER BY instrument_time
 	       rows between 120 preceding and 120 following);
-create index campbell_medians_wfms_idx on campbell_medians_wfms(site_id, measurement, instrument_time);
+create index campbell_medians_wfms_idx on campbell_medians_wfms(measurement_type_id, instrument_time);
 
 CREATE materialized VIEW campbell_medians_wfml AS
   select *,
@@ -68,70 +70,64 @@ CREATE materialized VIEW campbell_medians_wfml AS
 	 case when remove_outliers then runmad(calibrated_value) over w
 	 else null end as running_mad
     from calibrated_campbell_wfml
-  WINDOW w AS (partition by site_id, measurement
+  WINDOW w AS (partition by measurement_type_id
 	       ORDER BY instrument_time
 	       rows between 120 preceding and 120 following);
-create index campbell_medians_wfml_idx on campbell_medians_wfml(site_id, measurement, instrument_time);
+create index campbell_medians_wfml_idx on campbell_medians_wfml(measurement_type_id, instrument_time);
 
 /* 3) QC checks.
 
 These views check for flag conditions. They rely on functions from
 flags.sql. The end result is the fully processed data. */
 CREATE materialized VIEW processed_campbell_wfms as
-  select site_id,
+  select measurement_type_id,
 	 instrument_time as time,
-	 measurement,
 	 calibrated_value as value,
-	 is_flagged(measurement, site_id, null, instrument_time,
+	 is_flagged(measurement_type_id, null, instrument_time,
 		    calibrated_value, flagged, running_median,
 		    running_mad) as flagged
     from campbell_medians_wfms;
-create index processed_campbell_wfms_idx on processed_campbell_wfms(site_id, measurement, time);
+create index processed_campbell_wfms_idx on processed_campbell_wfms(measurement_type_id, time);
 
 CREATE materialized VIEW processed_campbell_wfml as
-  select site_id,
+  select measurement_type_id,
 	 instrument_time as time,
-	 measurement,
 	 calibrated_value as value,
-	 is_flagged(measurement, site_id, null, instrument_time,
+	 is_flagged(measurement_type_id, null, instrument_time,
 		    calibrated_value, flagged, running_median,
 		    running_mad) as flagged
     from campbell_medians_wfml;
-create index processed_campbell_wfml_idx on processed_campbell_wfml(site_id, measurement, time);
+create index processed_campbell_wfml_idx on processed_campbell_wfml(measurement_type_id, time);
 
 /* 4) Hourly aggragates.
 
 This view aggregates the processed data by hour. It relies on a
 function from flags.sql. */
 CREATE materialized VIEW hourly_campbell_wfms as
-  select site_id,
+  select measurement_type_id,
 	 time,
-	 measurement,
 	 value,
-	 get_hourly_flag(site_id, measurement, value, n_values::int) as flag
-    from (select site_id,
+	 get_hourly_flag(measurement_type_id, value, n_values::int) as flag
+    from (select measurement_type_id,
 		 time_bucket('1 hour', time) as time,
-		 measurement,
 		 avg(value) FILTER (WHERE not flagged) as value,
 		 count(value) FILTER (WHERE not flagged) as n_values
 	    from processed_campbell_wfms
-	   group by site_id, measurement, time_bucket('1 hour', time)) c1;
-create index hourly_campbell_wfms_idx on hourly_campbell_wfms(site_id, measurement, time);
+	   group by measurement_type_id, time_bucket('1 hour', time)) c1;
+create index hourly_campbell_wfms_idx on hourly_campbell_wfms(measurement_type_id, time);
 
 CREATE materialized VIEW hourly_campbell_wfml as
-  select site_id,
+  select measurement_type_id,
 	 time,
-	 measurement,
 	 value,
-	 get_hourly_flag(site_id, measurement, value, n_values::int) as flag
-    from (select site_id,
+	 get_hourly_flag(measurement_type_id, value, n_values::int) as flag
+    from (select measurement_type_id,
 		 time_bucket('1 hour', time) as time,
-		 measurement,
 		 avg(value) FILTER (WHERE not flagged) as value,
 		 count(value) FILTER (WHERE not flagged) as n_values
 	    from processed_campbell_wfml
-	   group by site_id, measurement, time_bucket('1 hour', time)) c1;
-create index hourly_campbell_wfml_idx on hourly_campbell_wfml(site_id, measurement, time);
+	   group by measurement_type_id, time_bucket('1 hour', time)) c1;
+create index hourly_campbell_wfml_idx on hourly_campbell_wfml(measurement_type_id, time);
 
 /* To update the processed data, simply need to refresh the relevant materialized views. For example, to update WFMS campbell results:
 refresh materialized view calibration_values;
