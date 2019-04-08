d4a7204bba59d73f802a07a037db5de7a27167b4
wmay <williamcmay@live.com>
2019-04-08 19:22:04 -0400

    Get rudimentary etl_extract working

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -1,12 +1,15 @@
 ## download data files
 
-list_html_files = function(url) {
-  df = xml2::read_html(url) %>%
+list_html_files = function(url, user, password) {
+  ## due to the password need to convert regular URL string to this
+  ## fancy rvest session
+  pw_config = httr::authenticate(user, password)
+  session = rvest::html_session(url, pw_config)
+  df = xml2::read_html(session) %>%
     rvest::html_table() %>%
     `[[`(1)
   df$Name[-c(1:2, nrow(df))]
 }
-
 
 #' My ETL functions
 #' @import etl
@@ -20,59 +23,48 @@ list_html_files = function(url) {
 #' }
 #' }
 etl_extract.etl_nysatmoschem <- function(obj, user, password, years, sites = NULL) {
-  ## stop('etl_extract is not yet implemented')
   if (is.null(user) || is.null(password)) {
     stop('Username and password required to access data.')
   }
   base_url = paste0('http://', user, ':', password,
-                    '@atmoschem.asrc.cestm.albany.edu/~aqm/AQM_Products/raw/')
+                    '@atmoschem.asrc.cestm.albany.edu/~aqm/AQM_Products/raw')
 
   ## if no site is specified, visit the website to get a list of sites
   if (is.null(sites)) {
-    sites = gsub('/$', '', list_html_files(base_url))
+    sites = gsub('/$', '', list_html_files(base_url, user, password))
   }
 
-  month_format = '%Y/%m'
-  date_format = '/%Y/%m/%Y%m%d_'
+  download_urls = character(0)
   for (site in sites) {
     site_url = paste(base_url, site, sep = '/')
-    ## get data sources
-    data_sources = gsub('/$', '', list_html_files(site_url))
-
-    ## for source in data sources
+    data_sources = gsub('/$', '', list_html_files(site_url, user, password))
     for (s in data_sources) {
       s_url = paste(site_url, s, sep = '/')
-      ## get files
-      files = list_html_files(s_url)
-
-      ## do something dependent on file type
-      ## ...
-    }
-
-
-    
-    for (n in 1:length(dates)) {
-      d = dates[n]
-      month_path = file.path(attr(obj, 'raw_dir'), site,
-                             format(d, format = month_format))
-      print(month_path)
-      dir.create(month_path, showWarnings = T, recursive = T)
-      date_path = format(d, format = date_format)
-      for (f in file_types) {
-        rel_path = paste0(site, date_path, f)
-        src = paste0(base_url, rel_path)
-        etl::smart_download(obj, src, rel_path)
+      files = list_html_files(s_url, user, password)
+      for (year in years) {
+        try_result = try(file_years <- extract_year(files, site, s))
+        if (class(try_result) == 'try-error') {
+          ## this means extract_year isn't implemented for that data
+          ## source
+          next()
+        }
+        year_files = files[file_years == year]
+        if (length(year_files) > 0) {
+          ## add urls to download list
+          file_urls = paste(base_url, site, s, year_files, sep = '/')
+          download_urls = c(download_urls, file_urls)
+          ## make sure local directory exists
+          dir.create(file.path(attr(obj, 'raw_dir'), site, s),
+                     showWarnings = FALSE, recursive = TRUE)
+        }
       }
     }
   }
 
-  
-  ## # Specify the URLs that you want to download
-  ## src <- c("http://www.stat.tamu.edu/~sheather/book/docs/datasets/HoustonChronicle.csv")
+  if (length(download_urls) > 0) {
+    rel_paths = gsub('.*/raw/', '', download_urls)
+    etl::smart_download(obj, download_urls, rel_paths)
+  }
 
-  ## # Use the smart_download() function for convenience
-  ## etl::smart_download(obj, src, ...)
-
-  # Always return obj invisibly to ensure pipeability!
   invisible(obj)
 }
