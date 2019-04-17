9d2aa0828e73381588980d93d9b3de971e120284
wmay <williamcmay@live.com>
2019-04-17 16:46:43 -0400

    Add PSP envidas files to extract_year

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,8 @@ extract_year = function(f, site, ds) {
                substr(gsub('^.*Table1_', '', f), 1, 4)
              } else if (site == 'WFMS' && ds == 'calibrations') {
                paste0('20', substr(gsub('^.*_', '', f), 1, 2))
+             } else if (site == 'PSP' && ds == 'envidas') {
+               paste0('20', substr(gsub('^.*-', '', f), 1, 2))
              } else {
                stop(paste('extract_year not implemented for',
                           site, 'and', ds))
