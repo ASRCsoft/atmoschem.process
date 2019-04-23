8e5db2cbdf98cbeefa82870633ba1c0a7f8c3f95
wmay <williamcmay@live.com>
2019-04-23 11:41:04 -0400

    Add TEI 43i SO2 146i calibrations

--- R/transform_psp_calibrations.R
+++ R/transform_psp_calibrations.R
@@ -35,7 +35,7 @@ read_psp_cal_times = function(f, date_cell = 'L11', start_cell = 'Z11',
 }
 
 ## get "zero and span checks" section
-read_psp_cal_table = function(pg, f, trange = 'F33:AD35',
+read_psp_NO_cal_table = function(pg, f, trange = 'F33:AD35',
                               chem_names = c('NO', 'NOx'),
                               cal_end) {
   ## gotta help out readxl a bit with the macro files
@@ -60,6 +60,23 @@ read_psp_cal_table = function(pg, f, trange = 'F33:AD35',
              corrected = FALSE)
 }
 
+## get "zero, span and PC checks" section
+read_psp_cal_table = function(pg, f, trange = 'U36:AA40',
+                              chem_name = 'CO', cal_end) {
+  df = readxl::read_xlsx(f, range = trange, col_names = F)
+  cals = as.data.frame(df[c(1, nrow(df)), c(1, ncol(df))])
+  names(cals) = c('provided_value', 'measured_value')
+  row.names(cals) = c('span', 'zero')
+  mtype_id = get_measurement_type_id(pg, 'PSP', 'envidas',
+                                     chem_name)
+  data.frame(measurement_type_id = mtype_id,
+             type = row.names(cals),
+             cal_time = cal_end,
+             provided_value = cals$provided_value,
+             measured_value = cals$measured_value,
+             corrected = FALSE)
+}
+
 empty_measurements = function() {
   data.frame(measurement_type_id = integer(0),
              type = character(0),
@@ -74,7 +91,7 @@ transform_psp_42C_calibrations = function(pg, f) {
   if (is.null(cal_end)) {
     return(empty_measurements())
   }
-  read_psp_cal_table(pg, f, trange = 'F33:AD35',
+  read_psp_NO_cal_table(pg, f, trange = 'F33:AD35',
                      chem_names = c('NO', 'NOx'),
                      cal_end)
 }
@@ -84,18 +101,8 @@ transform_psp_API300EU_calibrations = function(pg, f) {
   if (is.null(cal_end)) {
     return(empty_measurements())
   }
-
-  ## get "zero and span checks" section
-  df = readxl::read_xlsx(f, range = 'U36:AA38', col_names = F)
-  cals = as.data.frame(df[c(1, 3), c(1, ncol(df))])
-  names(cals) = c('provided_value', 'measured_value')
-  row.names(cals) = c('zero', 'span')
-  data.frame(measurement_type_id = get_measurement_type_id(pg, 'PSP', 'envidas', 'CO'),
-             type = row.names(cals),
-             cal_time = cal_end,
-             provided_value = cals$provided_value,
-             measured_value = cals$measured_value,
-             corrected = FALSE)
+  read_psp_cal_table(pg, f, trange = 'U38:AA42',
+                     chem_name = 'CO', cal_end)
 }
 
 transform_psp_ASRC_TEI42i_Y_NOy_146i_calibrations = function(pg, f) {
@@ -103,9 +110,9 @@ transform_psp_ASRC_TEI42i_Y_NOy_146i_calibrations = function(pg, f) {
   if (is.null(cal_end)) {
     return(empty_measurements())
   }
-  read_psp_cal_table(pg, f, trange = 'F33:AD35',
-                     chem_names = c('NO-ASRC', 'NOy-ASRC'),
-                     cal_end)
+  read_psp_NO_cal_table(pg, f, trange = 'F33:AD35',
+                        chem_names = c('NO-ASRC', 'NOy-ASRC'),
+                        cal_end)
 }
 
 transform_psp_DEC_TEI42i_NOy_calibrations = function(pg, f) {
@@ -113,9 +120,18 @@ transform_psp_DEC_TEI42i_NOy_calibrations = function(pg, f) {
   if (is.null(cal_end)) {
     return(empty_measurements())
   }
-  read_psp_cal_table(pg, f, trange = 'F34:AD36',
-                     chem_names = c('NO-DEC', 'NOy-DEC'),
-                     cal_end)
+  read_psp_NO_cal_table(pg, f, trange = 'F34:AD36',
+                        chem_names = c('NO-DEC', 'NOy-DEC'),
+                        cal_end)
+}
+
+transform_psp_TEI43i_SO2_146i_calibrations = function(pg, f) {  
+  cal_end = read_psp_cal_times(f)$end_time
+  if (is.null(cal_end)) {
+    return(empty_measurements())
+  }
+  read_psp_cal_table(pg, f, trange = 'U36:AA40',
+                     chem_name = 'SO2', cal_end)
 }
 
 transform_psp_calibrations = function(pg, f) {
@@ -128,6 +144,8 @@ transform_psp_calibrations = function(pg, f) {
     transform_psp_ASRC_TEI42i_Y_NOy_146i_calibrations(pg, f)
   } else if (is_psp_DEC_TEI42i_NOy_cal(f)) {
     transform_psp_DEC_TEI42i_NOy_calibrations(pg, f)
+  } else if (is_psp_TEI43i_SO2_146i_cal(f)) {
+    transform_psp_TEI43i_SO2_146i_calibrations(pg, f)
   } else {
     warning(paste('Transform not implemented for', f))
   }
--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,8 @@ is_psp_ASRC_TEI42i_Y_NOy_146i_cal = function(f)
   startsWith(basename(f), 'Pinnacle_ASRC_TEI42i_Y_NOy_146i_Weekly')
 is_psp_DEC_TEI42i_NOy_cal = function(f)
   startsWith(basename(f), 'Pinnacle DEC TEI42i NOy Weekly')
+is_psp_TEI43i_SO2_146i_cal = function(f)
+  startsWith(basename(f), 'Pinnacle_TEI43i_SO2_146i_Weekly')
 
 extract_psp_calibrations_year = function(f) {
   if (is_psp_42C_cal(f)) {
@@ -18,6 +20,8 @@ extract_psp_calibrations_year = function(f) {
     gsub('^.*Weekly_|_[^_].*$', '', f)
   } else if (is_psp_DEC_TEI42i_NOy_cal(f)) {
     gsub('^.* |_.*$', '', f)
+  } else if (is_psp_TEI43i_SO2_146i_cal(f)) {
+    gsub('^.*Weekly_|_[^_].*$', '', f)
   } else {
     warning(paste('extract_year not implemented for PSP calibration', f))
     NA
