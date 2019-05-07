f30966d84793679c68138438b533425713b4c00b
wmay <williamcmay@live.com>
2019-05-07 15:35:05 -0400

    Get CE values from PSP calibration sheets

--- R/transform_psp_calibrations.R
+++ R/transform_psp_calibrations.R
@@ -37,24 +37,50 @@ read_psp_cal_times = function(f, date_cell = 'L11', start_cell = 'Z11',
 ## get "zero and span checks" section
 read_psp_NO_cal_table = function(f, trange = 'F33:AD35',
                                  chem_names = c('NO', 'NOx'),
-                                 cal_end) {
+                                 cal_end, ce_row = 42) {
   ## gotta help out readxl a bit with the macro files
   if (endsWith(f, 'm')) {
-    df = readxl::read_xlsx(f, range = trange, col_names = F)
+    readf = readxl::read_xlsx
   } else {
-    df = readxl::read_xls(f, range = trange, col_names = F)
+    readf = readxl::read_xls
   }
+  ## first the zero and span
+  df = readf(f, range = trange, col_names = F)
   cals = as.data.frame(df[c(1, 3), c(1, 8, 14)])
   names(cals) = c('cert_span', 'zero', 'span')
   row.names(cals) = chem_names
   cal_df = expand.grid(chem_names, c('zero', 'span'))
   names(cal_df) = c('measurement_name', 'type')
-  data.frame(measurement_name = cal_df$measurement_name,
-             type = cal_df$type,
-             cal_time = cal_end,
-             provided_value = c(rep(0, 2), cals$cert_span),
-             measured_value = cals[as.matrix(cal_df[, 1:2])],
-             corrected = FALSE)
+  res1 = data.frame(measurement_name = cal_df$measurement_name,
+                    type = cal_df$type,
+                    cal_time = cal_end,
+                    provided_value = c(rep(0, 2), cals$cert_span),
+                    measured_value = cals[as.matrix(cal_df[, 1:2])],
+                    corrected = FALSE)
+  ## and now the conversion efficiencies
+  ce_range = paste0('O', ce_row, ':W', ce_row)
+  df2 = readf(f, range = ce_range, col_names = F)
+  ces = as.data.frame(df2[, c(1, 9)])
+  names(ces) = c('calibrator', 'response')
+  responses = strsplit(as.character(ces$response), ' +')[[1]]
+  if (length(responses) == 1) {
+    res2 = data.frame(measurement_name = cal_df$measurement_name[2],
+                      type = 'CE',
+                      cal_time = cal_end,
+                      provided_value = ces$calibrator,
+                      measured_value = ces$response,
+                      corrected = FALSE)
+  } else if (length(responses) == 2) {
+    res2 = data.frame(measurement_name = cal_df$measurement_name[1:2],
+                      type = 'CE',
+                      cal_time = cal_end,
+                      provided_value = ces$calibrator,
+                      measured_value = responses,
+                      corrected = FALSE)
+  } else {
+    stop("Can't parse conversion efficiencies.")
+  }
+  rbind(res1, res2)
 }
 
 ## get "zero, span and PC checks" section
@@ -84,14 +110,15 @@ empty_measurements = function() {
 transform_psp_NO_calibrations = function(f, date_cell = 'L11',
                                          start_cell = 'Z11', end_cell = 'AE11',
                                          trange = 'F33:AD35',
-                                         chem_names = c('NO', 'NOx')) {
+                                         chem_names = c('NO', 'NOx'),
+                                         ce_row = 42) {
   cal_end = read_psp_cal_times(f, date_cell, start_cell,
                                end_cell)$end_time
   if (is.null(cal_end)) {
     return(empty_measurements())
   }
   read_psp_NO_cal_table(f, trange, chem_names,
-                        cal_end)
+                        cal_end, ce_row)
 }
 
 transform_psp_single_calibrations = function(f, date_cell = 'L11',
@@ -120,12 +147,14 @@ transform_psp_API300EU_calibrations = function(f) {
 
 transform_psp_ASRC_TEI42i_Y_NOy_calibrations = function(f) {
   transform_psp_NO_calibrations(f,
-                                chem_names = c('NOy-HNO3', 'NOy'))
+                                chem_names = c('NOy-HNO3', 'NOy'),
+                                ce_row = 45)
 }
 
 transform_psp_DEC_TEI42i_NOy_calibrations = function(f) {
   transform_psp_NO_calibrations(f, 'K11', trange = 'F34:AD36',
-                                chem_names = c('NO-DEC', 'NOy-DEC'))
+                                chem_names = c('NO-DEC', 'NOy-DEC'),
+                                ce_row = 46)
 }
 
 transform_psp_TEI43i_SO2_calibrations = function(f) {  
