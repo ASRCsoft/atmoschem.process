ef4618e2c8852b1f35e964b2ad61ed0742910acf
William May <williamcmay@live.com>
2019-06-16 02:29:58 -0400

    Update etl_extract for new data organization

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -22,8 +22,8 @@ list_html_files = function(url, user, password) {
 #'     etl_create()
 #' }
 #' }
-etl_extract.etl_nysatmoschem <- function(obj, user, password, sites = NULL,
-                                         data_sources = NULL, years = NULL, ...) {
+etl_extract.etl_nysatmoschem = function(obj, user, password, sites = NULL,
+                                        data_sources = NULL, years = NULL, ...) {
   if (is.null(user) || is.null(password)) {
     stop('Username and password required to access data.')
   }
@@ -35,40 +35,54 @@ etl_extract.etl_nysatmoschem <- function(obj, user, password, sites = NULL,
     sites = gsub('/$', '', list_html_files(base_url, user, password))
   }
 
-  ## download_urls = character(0)
   for (site in sites) {
     site_url = paste(base_url, site, sep = '/')
-    site_data_sources = gsub('/$', '', list_html_files(site_url, user, password))
+    measurements_url = paste(site_url, 'measurements', sep = '/')
+    site_data_sources = gsub('/$', '', list_html_files(measurements_url, user, password))
     if (!is.null(data_sources)) {
       site_data_sources = site_data_sources[site_data_sources %in% data_sources]
     }
-    for (s in site_data_sources) {
-      s_url = paste(site_url, s, sep = '/')
-      files = list_html_files(s_url, user, password)
-      try_result = try(file_years <- extract_year(files, site, s))
-      if (class(try_result) == 'try-error') {
-        ## this means extract_year isn't implemented for that data
-        ## source
-        next()
+    for (ds in site_data_sources) {
+      ds_url = paste(measurements_url, ds, sep = '/')
+      data_source_years = gsub('/$', '', list_html_files(ds_url, user, password))
+      if (!is.null(years)) {
+        data_source_years = data_source_years[as.integer(data_source_years) %in% years]
       }
-      if (is.null(years)) {
-        year_files = files[!is.na(file_years)]
-      } else {
-        year_files = files[file_years %in% years]
-      }
-      if (length(year_files) > 0) {
-        message(paste('Extracting', site, '/', s, 'files...', sep = ' '))
-        ## add urls to download list
-        file_urls = paste(base_url, site, s, year_files, sep = '/')
+      for (y in data_source_years) {
+        y_url = paste(ds_url, y, sep = '/')
+        message(paste('Downloading', site, '/', ds, '/', y, 'files...', sep = ' '))
+        year_files = list_html_files(y_url, user, password)
+        file_urls = paste(y_url, year_files, sep = '/')
         rel_paths = gsub('.*/raw/', '', file_urls)
         ## make sure local directory exists
-        dir.create(file.path(attr(obj, 'raw_dir'), site, s),
+        dir.create(file.path(attr(obj, 'raw_dir'), site, 'measurements', ds, y),
+                   showWarnings = FALSE, recursive = TRUE)
+        etl::smart_download(obj, sapply(file_urls, URLencode),
+                            rel_paths, ...)
+      }
+    }
+    ## also get the calibrations for each site
+    calibrations_url = paste(site_url, 'calibrations', sep = '/')
+    site_instruments = gsub('/$', '', list_html_files(calibrations_url, user, password))
+    for (instr in site_instruments) {
+      instr_url = paste(calibrations_url, instr, sep = '/')
+      instrument_years = gsub('/$', '', list_html_files(instr_url, user, password))
+      if (!is.null(years)) {
+        instrument_years = instrument_years[as.integer(instrument_years) %in% years]
+      }
+      for (y in instrument_years) {
+        y_url = paste(instr_url, y, sep = '/')
+        message(paste('Downloading', site, '/', instr, '/', y, 'calibration files...', sep = ' '))
+        year_files = list_html_files(y_url, user, password)
+        file_urls = paste(y_url, year_files, sep = '/')
+        rel_paths = gsub('.*/raw/', '', file_urls)
+        ## make sure local directory exists
+        dir.create(file.path(attr(obj, 'raw_dir'), site, 'calibrations', instr, y),
                    showWarnings = FALSE, recursive = TRUE)
         etl::smart_download(obj, sapply(file_urls, URLencode),
                             rel_paths, ...)
       }
     }
   }
-
   invisible(obj)
 }
