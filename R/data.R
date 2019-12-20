cb7d15d3126423412ab63a895426b09b1fd7050c
wmay <williamcmay@live.com>
2019-12-20 18:01:49 -0500

    Use data-raw to organize supporting datasets

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' The instrument auto-calibration schedule
+"autocals"
+
+#' Calibration flags
+"cal_flags"
+
+#' Clock audit results
+"clock_audits"
+
+#' Instruments
+"instruments"
+
+#' Measurement flags
+"manual_flags"
+
+#' Measurement instruments
+"measurement_sources"
+
+#' Information and processing instructions for each type of
+#' measurement
+"measurement_types"
+
+#' Report column information
+"report_columns"
---
+++ data-raw/package_data.R
@@ -0,0 +1,17 @@
+## move supporting datasets (in data-raw/csv) to /data
+
+library(usethis)
+
+csv_files = list.files('data-raw/package_data', pattern = '\\.csv$',
+                       full.names = TRUE)
+
+## usethis::use_data requires passing the data as a list of symbols,
+## so we have to assign the data frames and pass a list of the data
+## frame names as symbols
+df_names = tools::file_path_sans_ext(basename(csv_files))
+for (n in 1:length(csv_files)) {
+  assign(df_names[n], read.csv(csv_files[n]))
+}
+dflist = lapply(df_names, as.symbol)
+dflist$overwrite = TRUE
+do.call(use_data, dflist)
