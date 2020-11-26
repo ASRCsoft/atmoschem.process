043dac4689a91216787d5fb04502650d67a50181
William May <wcmay@albany.edu>
2020-11-25 23:59:43 -0500

    Add res_wind_speed and res_wind_dir

--- R/derived_values.R
+++ R/derived_values.R
@@ -14,6 +14,58 @@ combine_measures = function(obj, site, data_source, m1, m2,
     collect()
 }
 
+#' Resultant wind speed and direction
+#'
+#' Calculate the resultant wind speed and direction from a collection of wind
+#' speeds and directions.
+#'
+#' Resultant wind speeds are vector-averaged. We first find the average values
+#' of the east-west and north-south components,
+#'
+#' \deqn{V_e = -\frac{1}{N} \sum u_i \sin(\theta_i)}{Ve = -sum(u_i * sin(theta_i)) / N}
+#' \deqn{V_n = -\frac{1}{N} \sum u_i \cos(\theta_i)}{Vn = -sum(u_i * cos(theta_i)) / N}
+#'
+#' where \eqn{u} is the speed and \eqn{\theta}{theta} is the direction. The
+#' resultant speed and direction can then be calculated with
+#'
+#' \deqn{U_R = \sqrt{V_e^2 + V_n^2}}{U_R = sqrt(Ve^2 + Vn^2)}
+#' \deqn{\theta_R = \textrm{arctan}(\frac{V_e}{V_n}) + 180}{theta_R = arctan(Ve/Vn) + 180}
+#'
+#' @param speed Wind speeds.
+#' @param dir Wind directions in degrees clockwise from north.
+#' @param na.rm A logical value indicating whether \code{NA} values should be
+#'   stripped before the computation proceeds.
+#' @return \code{res_wind_speed} returns the resultant wind
+#'   speed. \code{res_wind_dir} returns the resultant wind direction.
+#' @examples
+#' # opposite winds cancel out
+#' res_wind_speed(1, c(0, 180))
+#'
+#' # A scalar average would return 180 in this case. Resultant wind direction
+#' # produces the correct value.
+#' res_wind_dir(1, c(355, 5))
+#'
+#' @references
+#'   \insertRef{us_environmental_protection_agency_meteorological_2000}{atmoschem.process}
+#' @export
+res_wind_speed = function(speed, dir, na.rm = TRUE) {
+  n = max(length(speed), length(dir))
+  theta = pi * dir / 180
+  Ve = -mean(speed * sin(theta), na.rm = na.rm)
+  Vn = -mean(speed * cos(theta), na.rm = na.rm)
+  sqrt(Ve^2 + Vn^2)
+}
+
+#' @rdname res_wind_speed
+#' @export
+res_wind_dir = function(speed, dir, na.rm = TRUE) {
+  n = max(length(speed), length(dir))
+  theta = pi * dir / 180
+  Ve = -mean(speed * sin(theta), na.rm = na.rm)
+  Vn = -mean(speed * cos(theta), na.rm = na.rm)
+  (180 * atan2(Ve, Vn) / pi + 180) %% 360
+}
+
 #' Sea level pressure
 #'
 #' Estimate sea level pressure from local pressure, temperature, and height
--- tests/testthat/test_derived.R
+++ tests/testthat/test_derived.R
@@ -1,5 +1,18 @@
 library(atmoschem.process)
 
+test_that('resultant average of a single value is the value', {
+  expect_equal(res_wind_speed(5, 180), 5)
+  expect_equal(res_wind_dir(1, 320), 320)
+})
+
+test_that('resultant of opposite winds is zero', {
+  expect_equal(res_wind_speed(1, c(0, 180)), 0)
+})
+
+test_that('resultant direction around 360/0 is correct', {
+  expect_equal(res_wind_dir(1, c(355, 5)), 0)
+})
+
 test_that('sea_level_pressure returns something reasonable', {
   # pressure should go up as height drops
   expect_gt(sea_level_pressure(800, 30, 1483.5), 800)
