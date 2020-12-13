c578892bf5388b6a18c34087faa83f7b261357a2
William May <wcmay@albany.edu>
2020-12-13 00:25:37 -0500

    Get PSP 42C conversion efficiencies in R

--- analysis/load_calibration.R
+++ analysis/load_calibration.R
@@ -51,66 +51,159 @@ get_cal_results = function(dt_int, t_int, p, d, agg_f) {
     transform(date = as.Date(time, tz = 'EST')) %>%
     aggregate(value ~ date, FUN = agg_f, data = .)
 }
+# R's lead and lag functions aren't so useful. These are better.
+lag2 = function(x, k = 1) c(rep(NA, k), head(x, -k))
+lead2 = function(x, k = 1) c(tail(x, -k), rep(NA, k))
+# Guess the conversion efficiency calibration time period using the recorded NOx
+# value from the cal sheet (noxval), the range of calibration times, and the
+# NO/NOx divergence
+guess_42C_ce = function(meas, noxval) {
+  candidates = meas %>%
+    transform(no2 = nox - no) %>%
+    # split measurements by discontinuities and give each group an ID
+    transform(dnox = nox - lag2(nox), dno2 = no2 - lag2(no2)) %>%
+    transform(discontinuous = abs(dnox) > 1 | abs(dno2) > 1) %>%
+    transform(new_group = discontinuous & !lag2(discontinuous)) %>%
+    transform(group = cumsum(!is.na(new_group) & new_group)) %>%
+    # remove short-lived groups
+    subset(!discontinuous) %>%
+    transform(stime = ave(time, group, FUN = min),
+              etime = ave(time, group, FUN = max)) %>%
+    subset(etime - stime >= as.difftime(10, units = 'mins')) %>%
+    # get calibration results for each group 
+    transform(nocal = ave(no, group, FUN = function(x) min_ma(x, 5)),
+              noxcal = ave(nox, group, FUN = function(x) max_ma(x, 5))) %>%
+    subset(select = c('stime', 'etime', 'nocal', 'noxcal')) %>%
+    unique %>%
+    transform(no2cal = noxcal - nocal) %>%
+    # remove zero calibrations
+    subset(nocal > .3) %>%
+    # make sure measured NOx value is within +/- 30% of the recorded NOx value
+    subset(abs(100 * noxcal / noxval - 100) < 30)
+  if (!nrow(candidates)) return(NA)
+  candidates[order(candidates$no2cal, decreasing = T), ] %>%
+    head(1) %>%
+    with(list(start_time = stime, end_time = etime, no = nocal))
+}
 
 # get the manual cals
 mcals = file.path('analysis', 'cleaned', 'raw_data', site, 'calibrations', '*',
                   '*', '*') %>%
   Sys.glob %>%
   lapply(read.csv) %>%
-  do.call(rbind, .)
-# split up awkwardly formatted times column
-mcals$start_time = int_start(as_interval(mcals$times))
-mcals$end_time = int_end(as_interval(mcals$times))
+  do.call(rbind, .) %>%
+  transform(times = as_interval(times))
+
+if (site == 'PSP') {
+  # find NO conversion efficiency results, which weren't recorded in the PSP cal
+  # sheets
+
+  # get NO and NOx from sqlite
+  dbpath = file.path('analysis', 'intermediate', 'raw_PSP_envidas.sqlite')
+  db = dbConnect(SQLite(), dbpath)
+  sql_str = 'select time, ? as no, ? as nox from measurements order by time asc'
+  sql = sqlInterpolate(db, sql_str, dbQuoteIdentifier(db, 'value.NO'),
+                       dbQuoteIdentifier(db, 'value.NOx'))
+  meas = dbGetQuery(db, sql)
+  dbDisconnect(db)
+  meas$time = as.POSIXct(meas$time, tz = 'EST')
+
+  # make a plausible guess about the 42C CE results, since they aren't recorded
+  # in the PSP cal sheets
+  ce_mcals = subset(mcals, measurement_name == 'NOx' & type == 'CE')
+  no_ces = meas %>%
+    transform(is_cal = time %within% as.list(ce_mcals$times)) %>%
+    transform(new_cal = is_cal & !lag2(is_cal)) %>%
+    transform(cal_id = cumsum(new_cal)) %>%
+    subset(is_cal) %>%
+    # guess 42C CE times for each calibration
+    split(., .$cal_id) %>%
+    lapply(function(x) {
+      # find the matching interval in ce_mcals
+      mcal_i = ce_mcals[x$time[1] %within% ce_mcals$times, ]
+      guess = guess_42C_ce(x, mcal_i$measured_value)
+      if (!is.list(guess)) {
+        data.frame(measurement_name = 'NO', type = 'CE',
+                   start_time = int_start(mcal_i$times),
+                   end_time = int_end(mcal_i$times),
+                   provided_value = mcal_i$provided_value, measured_value = NA)
+      } else {
+        data.frame(measurement_name = 'NO', type = 'CE',
+                   start_time = guess$start_time, end_time = guess$end_time,
+                   provided_value = mcal_i$provided_value,
+                   measured_value = guess$no)
+      }
+    }) %>%
+    do.call(rbind, .) %>%
+    # fix time formatting messed up by rbind
+    transform(start_time = as.POSIXct(start_time, tz = 'EST', origin = '1970-01-01'),
+              end_time = as.POSIXct(end_time, tz = 'EST', origin = '1970-01-01')) %>%
+    transform(times = interval(start_time, end_time))
+  no_ces = no_ces[, !names(no_ces) %in% c('start_time', 'end_time')]
+  no_ces = no_ces[, c(1:2, 5, 3:4)]
+  no_ces$corrected = F
+  
+  # add NO CE values to the other manual cals
+  mcals = rbind(mcals, no_ces)
+}
+
+# fix up mcal formatting
+mcals = mcals %>%
+  transform(start_time = int_start(times),
+            end_time = int_end(times))
 mcals$times = NULL
 mcals$manual = T
-
-# get PSP NO conversion efficiencies
-# ...
-# guess_42C_ce_time, psp_no_ces
 
 # get the automated cals
 # first let's get a data frame of the cal times
 acals0 = autocals %>%
   transform(dt_int = as_interval(dates))
 acals0 = acals0[acals0$site == site, ]
-# for ongoing schedules use the current time as the end
-int_end(acals0$dt_int[is.na(int_end(acals0$dt_int))]) = Sys.time()
-acals_list = list()
-for (i in 1:nrow(acals0)) {
-  acalsi = acals0[i, ]
-  agg_f = switch(acalsi$type, zero = function(x) min_ma(x, 5),
-                 span = function(x) max_ma(x, 5),
-                 CE = function(x) max_ma(x, 5), function(x) NA)
-  dt_intp3 = pad_interval(acalsi$dt_int, as.difftime(3, units = 'mins'),
-                          as.difftime(3, units = 'mins'))
-  # spans calibrations tend to spike at the beginning, so remove the first 15
-  # minutes
-  if (acalsi$type == 'span') {
-    dt_intp3 = pad_interval(dt_intp3, as.difftime(-15, units = 'mins'), 0)
+if (nrow(acals0)) {
+  # for ongoing schedules use the current time as the end
+  int_end(acals0$dt_int[is.na(int_end(acals0$dt_int))]) = Sys.time()
+  acals_list = list()
+  for (i in 1:nrow(acals0)) {
+    acalsi = acals0[i, ]
+    agg_f = switch(acalsi$type, zero = function(x) min_ma(x, 5),
+                   span = function(x) max_ma(x, 5),
+                   CE = function(x) max_ma(x, 5), function(x) NA)
+    dt_intp3 = pad_interval(acalsi$dt_int, as.difftime(3, units = 'mins'),
+                            as.difftime(3, units = 'mins'))
+    # spans calibrations tend to spike at the beginning, so remove the first 15
+    # minutes
+    if (acalsi$type == 'span') {
+      dt_intp3 = pad_interval(dt_intp3, as.difftime(-15, units = 'mins'), 0)
+    }
+    c1 = with(acalsi, get_cal_results(dt_intp3, times, measurement_name,
+                                      data_source, agg_f))
+    if (nrow(c1) == 0) next()
+    names(c1)[2] = 'measured_value'
+    stime = hm(gsub('^[[(]|,.*$', '', acalsi$times))
+    etime = hm(gsub('^.*, ?|[])]$', '', acalsi$times))
+    c1$start_time = as.POSIXct(as.character(c1$date), tz = 'EST') + stime
+    c1$end_time = as.POSIXct(as.character(c1$date), tz = 'EST') + etime
+    c1$date = NULL
+    c1$measurement_name = acalsi$measurement_name
+    c1$data_source = acalsi$data_source
+    c1$type = acalsi$type
+    c1$provided_value = acalsi$value
+    acals_list[[i]] = c1
   }
-  c1 = with(acalsi, get_cal_results(dt_intp3, times, measurement_name,
-                                    data_source, agg_f))
-  if (nrow(c1) == 0) next()
-  names(c1)[2] = 'measured_value'
-  stime = hm(gsub('^[[(]|,.*$', '', acalsi$times))
-  etime = hm(gsub('^.*, ?|[])]$', '', acalsi$times))
-  c1$start_time = as.POSIXct(as.character(c1$date), tz = 'EST') + stime
-  c1$end_time = as.POSIXct(as.character(c1$date), tz = 'EST') + etime
-  c1$date = NULL
-  c1$measurement_name = acalsi$measurement_name
-  c1$data_source = acalsi$data_source
-  c1$type = acalsi$type
-  c1$provided_value = acalsi$value
-  acals_list[[i]] = c1
-}
-acals = do.call(rbind, acals_list)
-acals$corrected = F
-acals$manual = F
+  acals = do.call(rbind, acals_list)
 
-# combine!
-all_cals = rbind(mcals, acals[, names(mcals)])
+  # add acals to the mcals
+  acals$corrected = F
+  acals$manual = F
+  all_cals = rbind(mcals, acals[, names(mcals)])
+} else {
+  all_cals = mcals
+}
 
 # write to sqlite file
+# fix time formatting for sqlite compatibility
+all_cals$start_time = format(all_cals$start_time, '%Y-%m-%d %H:%M:%S', tz = 'EST')
+all_cals$end_time = format(all_cals$end_time, '%Y-%m-%d %H:%M:%S', tz = 'EST')
 interm_dir = file.path('analysis', 'intermediate')
 dir.create(interm_dir, F, T)
 dbpath = paste0('cals_', site, '.sqlite') %>%
