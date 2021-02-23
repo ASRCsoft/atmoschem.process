1ae20749ef58786d69d0d02a6f2bcc9a75b6ff11
William May <wcmay@albany.edu>
2021-02-23 14:46:48 -0500

    Rewrite test_config for tinytest

---
+++ inst/tinytest/test_config.R
@@ -0,0 +1,4 @@
+flag_periods = atmoschem.process:::as_interval(manual_flags$times)
+time_start = lubridate::int_start(flag_periods)
+time_end = lubridate::int_end(flag_periods)
+expect_true(all(time_start < time_end), 'manual_flags time periods make sense')
