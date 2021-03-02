392ba697e9ef2e64ba534a9622be01d0bac5ed32
William May <wcmay@albany.edu>
2021-03-02 15:10:29 -0500

    Move config tests to analysis/tests

--- analysis/tests/test_config.R
+++ analysis/tests/test_config.R
@@ -1,4 +1,8 @@
-flag_periods = atmoschem.process:::as_interval(manual_flags$times)
+library(atmoschem.process)
+
+config = read_csv_dir('../config')
+
+flag_periods = atmoschem.process:::as_interval(config$manual_flags$times)
 time_start = lubridate::int_start(flag_periods)
 time_end = lubridate::int_end(flag_periods)
 expect_true(all(time_start < time_end), 'manual_flags time periods make sense')
