67e8c54b3fb66661f32859bf9cef576a085bc0fc
William May <wcmay@albany.edu>
2021-03-26 01:59:37 -0400

    Add to AQS/NARSTO flag dictionary

--- data-raw/package_data.R
+++ data-raw/package_data.R
@@ -4,4 +4,9 @@ library(usethis)
 
 narsto_flags = read.csv('data-raw/narsto_flags.csv', stringsAsFactors = F)
 aqs_flags = read.csv('data-raw/aqs_flags.csv', stringsAsFactors = F)
+
+# add narsto flags to aqs data
+aqs_narsto = read.csv('data-raw/aqs_narsto.csv', stringsAsFactors = F)
+aqs_flags = merge(aqs_flags, aqs_narsto, by.x = 'code', by.y = 'aqs')
+
 use_data(narsto_flags, aqs_flags, overwrite = T)
