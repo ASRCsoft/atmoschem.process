b9912f45433606ddc6b7f99f8e3e82db4d0c7039
William May <wcmay@albany.edu>
2021-10-27 03:44:34 -0400

    Update cal file tests

--- inst/tinytest/test_asrc.R
+++ inst/tinytest/test_asrc.R
@@ -5,7 +5,13 @@ for (f in list.files('asrc_cals', full.names = T)) {
   site = switch(model, '42i' = 'WFML', '48C' = 'WFML', 'WFMS')
   brand = switch(model, '300EU' = 'Teledyne', 'Thermo')
   instrument = paste(brand, model, sep = '_')
-  cal = atmoschem.process:::transform_calibration(f, site, instrument)
-  info = paste('transform_calibration parses', basename(f))
+  transform_fun = switch(model, '42C' = 'transform_wfms_42C',
+                         '42Cs' = 'transform_wfms_42Cs',
+                         '42i' = 'transform_wfml_42i',
+                         '43C' = 'transform_wfms_43C',
+                         '48C' = 'transform_wfml_48C',
+                         '300EU' = 'transform_wfms_300EU')
+  cal = get(transform_fun)(f)
+  info = paste(transform_fun, 'parses', basename(f))
   expect_true(nrow(cal) > 0, info = info)
 }
--- inst/tinytest/test_dec.R
+++ inst/tinytest/test_dec.R
@@ -1,7 +1,14 @@
 library(atmoschem.process)
 
 for (f in list.files('dec_cals', full.names = T)) {
-  cal = atmoschem.process:::transform_calibration(f, 'PSP')
-  info = paste('transform_calibration parses', basename(f))
+  model = gsub('Pinnacle_(ASRC_|DEC_)?([^_]+).*', '\\1\\2', basename(f))
+  transform_fun = switch(model, '42C' = 'transform_psp_42C_calibrations',
+                         'API300EU' = 'transform_psp_API300EU_calibrations',
+                         'ASRC_TEI42i' = 'transform_psp_ASRC_TEI42i_Y_NOy_calibrations',
+                         'DEC_TEI42i' = 'transform_psp_DEC_TEI42i_NOy_calibrations',
+                         'TEI43i' = 'transform_psp_TEI43i_SO2_calibrations',
+                         'TEI49i' = 'transform_psp_TEI49i_O3_49i_calibrations')
+  cal = get(transform_fun)(f)
+  info = paste(transform_fun, 'parses', basename(f))
   expect_true(nrow(cal) > 0, info = info)
 }
